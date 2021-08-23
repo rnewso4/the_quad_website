bf5c4cafcb483ccba2142fc97311792834dd0ad5
rnewso4 <rnewso4@lsu.edu>
2021-08-23 14:32:01 -0500

    getEvents added

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,8 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+const { firestore } = require("firebase-admin");
+
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +11,15 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   response.send("Hello from Firebase!");
 });
+
+exports.getEvents = functions.https.onRequest((req, res) => {
+    admin.firestore().collection('events').get()
+    .then((data) => {
+        let events = [];
+        data.forEach(doc => {
+            events.push(doc.data());
+        });
+        return res.json(events);
+    })
+    .catch(err => console.error(err));
+})
